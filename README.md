d1c7a3c5a001269cc757652a49bded584bf6c5f1
Mikko Leino <iammikkoleino@gmail.com>
2012-11-04 12:59:55 +0200

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+webscarep-setup
+===============
+
+Sets up a node + domjs + jquery webscraping environment for you, ie. installs necessary node modules, downloads newest jquery and hands you an example script.
+
+## Usage
+
+Easiest (and totally safe) way to use this script: 
+
+    curl -fsSkL raw.github.com/mjleino/webscrape-setup/master/scrape-setup.sh | sh
+
+## Props
+
+Thanks to Charlies Robbins, [the script is based on his blog post](http://blog.nodejitsu.com/jsdom-jquery-in-5-lines-on-nodejs).
---
+++ scrape-setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# sets up a nice webscraping environment
+# based on http://blog.nodejitsu.com/jsdom-jquery-in-5-lines-on-nodejs
+#
+# dependencies: npm
+#
+# usage: ./scrape-setup.sh [WHERE]
+
+command -v npm >/dev/null 2>&1 || { 
+	echo >&2 "npm is required, https://npmjs.org/" 
+	exit 1
+}
+
+[ $# -gt 0 ] && {
+	mkdir -p $1 >& /dev/null || {
+		echo >&2 "$1 is not a directory"
+		exit 2
+	}
+	cd $1
+}
+
+npm install jsdom request
+curl -O http://code.jquery.com/jquery.min.js
+cat >example.js <<EXAMPLESCRIPT
+var request = require('request'),
+    jsdom = require('jsdom');
+request({ uri: "http://viiksipojat.fi" }, function (error, response, body) {
+  if (error && response.statusCode !== 200) {
+    console.log('Oops, error.')
+  }
+  jsdom.env({
+    html: body,
+    scripts: ["jquery.min.js"]
+  }, function (err, window) {
+// --------------
+// THE BEEF
+var $ = window.jQuery;
+console.log(\$('body').html());
+// --------------
+  });
+});
+EXAMPLESCRIPT
+
+echo your beautiful webscraping environment is ready at $(pwd)
+echo next step: node example.js
